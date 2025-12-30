834fb55edbfd98b278a365dd5c5ed6b116509dc0
Serosh-commits <REPLACE_WITH_YOUR_GITHUB_EMAIL>
2025-12-30 18:56:47 +0530

    Add array_fixed_variable_v2.cpp - Level 1 Implementation

---
+++ code/level1/array_fixed_variable_v2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int fixed_arr[5];
+    int n;
+    cin >> n;
+    int* var_arr = (int*)malloc(n * sizeof(int));
+    free(var_arr);
+    return 0;
+}
