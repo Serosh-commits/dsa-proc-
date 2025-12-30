05356bc7c88f42b6674bb53d017ba24398499e7a
Serosh-commits <REPLACE_WITH_YOUR_GITHUB_EMAIL>
2025-12-30 18:58:33 +0530

    Add string_basic_ops.cpp - Level 1 Implementation

---
+++ code/level1/string_basic_ops.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "hello";
+    cout << s.length() << endl;
+    string s2 = s + " world";
+    string s3 = s2;
+    return 0;
+}
