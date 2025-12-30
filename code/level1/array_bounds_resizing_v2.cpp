e96c45d909de499ce777422f0e72fc3bd228521d
Serosh-commits <REPLACE_WITH_YOUR_GITHUB_EMAIL>
2025-12-30 18:56:34 +0530

    Add array_bounds_resizing_v2.cpp - Level 1 Implementation

---
+++ code/level1/array_bounds_resizing_v2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++) arr[i] = i;
+    int new_n = 10;
+    int* new_arr = new int[new_n];
+    for(int i = 0; i < n; i++) new_arr[i] = arr[i];
+    delete[] arr;
+    arr = new_arr;
+    for(int i = 0; i < new_n; i++) cout << arr[i] << " ";
+    return 0;
+}
