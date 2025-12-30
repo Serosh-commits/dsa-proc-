e6135b5c3f1fc2fc3223038b713cfba9f056f115
Serosh-commits <REPLACE_WITH_YOUR_GITHUB_EMAIL>
2025-12-30 18:57:37 +0530

    Add math_gcd_lcm.cpp - Level 1 Implementation

---
+++ code/level1/math_gcd_lcm.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+
+int main() {
+    cout << gcd(12, 18) << " " << lcm(12, 18) << endl;
+    return 0;
+}
