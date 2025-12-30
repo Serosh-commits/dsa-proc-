a4e8327b9881d3f94c58e2bce34372b136a8f5bc
Serosh-commits <REPLACE_WITH_YOUR_GITHUB_EMAIL>
2025-12-30 18:56:50 +0530

    Add array_linear_search.cpp - Level 1 Implementation

---
+++ code/level1/array_linear_search.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key) {
+    for(int i = 0; i < n; i++) if(arr[i] == key) return i;
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << linearSearch(arr, 5, 3) << endl;
+    return 0;
+}
