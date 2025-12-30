b797384c73f366e6619ef367b526b2ea3dd02348
Serosh-commits <REPLACE_WITH_YOUR_GITHUB_EMAIL>
2025-12-30 18:58:14 +0530

    Add stack_basic.cpp - Level 1 Implementation

---
+++ code/level1/stack_basic.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+    for(char c : s) {
+        if(c == '(' || c == '[' || c == '{') st.push(c);
+        else {
+            if(st.empty()) return false;
+            if(c == ')' && st.top() != '(') return false;
+            if(c == ']' && st.top() != '[') return false;
+            if(c == '}' && st.top() != '{') return false;
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    cout << isValid("()[]{}") << endl;
+    return 0;
+}
